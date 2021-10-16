b8bec8e407b17dc3f2106708ca4d7b08bdee8d5e
曾詩翰 <gooddennis13@gmail.com>
2021-10-16 22:41:35 +0800

    部署上傳

---
+++ README.md
@@ -0,0 +1,19 @@
+# flask_api_server
+
+## Installation
+套件環境版本安裝 requirements.txt
+```bash
+pip3 install -r requirements.txt
+```
+
+## start
+啟動程式
+```bash
+python3 application.py
+```
+
+## API doc
+API Swagger 文件，在網址後面打上/api/doc
+```bash
+http://網址/api/doc
+```
---
+++ application.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from flasgger import Swagger
+from flasgger.utils import swag_from
+import os
+
+application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = './static'
+
+# Create an APISpec
+template = {
+    "info": {
+        "title": "Flask Restful Swagger API doc",
+        "description": "A Demof for the Flask-Restful Swagger API doc",
+        "version": "0.0.1",
+        "contact": {
+            "name": "",
+            "url": "",
+        }
+    },
+    "security": [
+        {
+            "Bearer": []
+        }
+    ]
+
+}
+application.config['SWAGGER'] = {
+    "specs_route": "/api/doc/"
+}
+CORS(application)
+Swagger(application, template=template)
+
+
+@application.route('/')
+def hello_world():  # put applicationlication's code here
+    return '110-DIGI-ITRI_API'
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@application.route('/api/photo', methods=['POST'])
+@swag_from("./api_doc/photo.yml")
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message': 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
+        resp = jsonify({'message': 'File successfully uploaded ' + filename})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp.status_code = 400
+        return resp
+
+
+if __name__ == '__main__':
+    from waitress import serve
+
+    serve(application, host="0.0.0.0", port=8080)
